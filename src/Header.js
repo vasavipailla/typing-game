bd41ea80399556d4ee4e445060926deb52903cd4
vasavipailla <vasavipatlolla@gmail.com>
2023-05-04 18:53:12 +0200

    header is added

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    return (
+
+        <header>
+        <nav>
+            <div className="nav_bar">
+                <div>
+                <p style={{fontStyle:"-moz-initial",fontSize:"30px",color:"p"}} className="logo">TYPING GAME</p>
+                </div>
+                <div>
+                <ul className="list">
+                    <li className="list-item">Sign-UP</li>
+                    <li className="list-item">Login-In</li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Header;
