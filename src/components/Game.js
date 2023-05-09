75963b9a82a2e5663dc7c4937fdcb24218609bb1
Tasneemhussain <tasneemhussain321@gmail.com>
2023-05-09 21:32:58 +0200

    added game page

---
+++ src/components/Game.js
@@ -0,0 +1,29 @@
+
+
+ const Game = () => {
+
+
+  return (
+   <>
+   
+   <h1>Wellcome To The Typing</h1>
+   <div className="game-container">
+        <div className="game-container2">
+        <h2>Typing Game</h2>
+            <h3>Follow the Typing word</h3>
+            <h1>"Word"</h1>
+            <input
+            type="text"
+            autoFocus
+            />
+            <p>score:<span>0</span></p>
+            <p>time:<span>0</span></p>
+        </div>
+
+    </div>
+   
+
+   </>
+  )
+}
+export default Game;
