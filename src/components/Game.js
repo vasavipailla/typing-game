50a9fe9fdd8059c46ff6d593a756c1331ce6de6d
Tasneemhussain <tasneemhussain321@gmail.com>
2023-05-09 21:46:26 +0200

    added styles

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,14 +10,15 @@
    <div className="game-container">
         <div className="game-container2">
         <h2>Typing Game</h2>
-            <h3>Follow the Typing word</h3>
+            <h3>Type The Following Word :</h3>
             <h1>"Word"</h1>
             <input
             type="text"
             autoFocus
+            placeholder="Type the word here"
             />
             <p>score:<span>0</span></p>
-            <p>time:<span>0</span></p>
+            <p>Time Left:<span>0</span></p>
         </div>
 
     </div>
