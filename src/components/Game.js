d9cc2f85718c9903ed9f46fba1f38b49b7245b1b
Tasneemhussain <tasneemhussain321@gmail.com>
2023-05-11 17:29:26 +0200

    added random word

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,21 @@
-
+import { useState,useEffect } from "react";
 
  const Game = () => {
+   const [words,setWords] = useState([])
 
+   const getRandomword = () => {
+    fetch("https://random-word-api.herokuapp.com/all")
+    .then(res => res.json())
+    .then(data => {
+        let randomword = Math.floor(Math.random() * data.length);
+        setWords(data[randomword]);
+    });
+   };
 
+    useEffect(() =>{
+       getRandomword();
+    },[]);
+   
   return (
    <>
    
@@ -11,14 +24,16 @@
         <div className="game-container2">
         <h2>Typing Game</h2>
             <h3>Type The Following Word :</h3>
-            <h1>"Word"</h1>
+            <h1>  {words} </h1>
             <input
             type="text"
             autoFocus
             placeholder="Type the word here"
+            onClick={getRandomword}
             />
             <p>score:<span>0</span></p>
             <p>Time Left:<span>0</span></p>
+           
         </div>
 
     </div>
